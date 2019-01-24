11732e96c26e14145e8bf15b8ea3738e65880abb
AlienNT <nick.tristen22@gmail.com>
2019-01-25 01:04:38 +0200

    Update main.js

--- FrontEnd/V_site/js/main.js
+++ FrontEnd/V_site/js/main.js
@@ -1,17 +1,34 @@
 ;(function ($) {
+    $(document).ready(function () {
+
+//Скролл
+
+        $(".scroll-down, .v_nav a").on("click", function (event) {
+            event.preventDefault();
+            var name = $(this).attr('href'),
+                top = $(name).offset().top;
+            $('body,html').animate({scrollTop: top}, 500);
+        });
+
+// Слайдер
 
 
-    $('.v_sect1_slider').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2700,
+        $('.v_sect1_slider').slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+
+        $('.slick-arrow , .slider-inner').css({
+            outline: 'none',
+            cursor: 'pointer'
+        });
+
+
     });
 
-    $('.slick-arrow , .slider-inner , .v_sect1_slider').css({
-        outline:'none',
-        cursor:'pointer'
-    })
 })(jQuery);
+
